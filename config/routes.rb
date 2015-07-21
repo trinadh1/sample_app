ae7fe7c6d6d0e09cd79d17ace60cce21a3434229
TRINADH1! <reddy.trinadhm50@gmail.com>
2015-07-21 19:45:33 +0530

    Add a static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'staticpages/home'
+
+  get 'staticpages/help'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'application#hello'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
